03205c6237f7ac73e276837cea850e39a3f54ee4
jianghui1103 <1219936992@qq.com>
2020-12-07 23:29:29 +0800

    增加配置

--- lib/index.tsx
+++ lib/index.tsx
@@ -2,9 +2,6 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import Icon from './icon'
 
-var childNode = document.createElement('div')
-childNode.setAttribute('id','root')
-document.body.appendChild(childNode)
 
 ReactDOM.render(<div>
     <Icon name="wechat"/>
