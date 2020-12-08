2945ff1cfae1f41a3cdc01f71146ab6e6a187e7c
jianghui1103 <1219936992@qq.com>
2020-12-08 23:34:14 +0800

    增加scss

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,18 @@ module.exports = {
             },{
                 test: /\.svg$/,
                 loader: 'svg-sprite-loader',
+            },{
+                test: /\.scss$/,
+                use: [
+                    // [style-loader](/loaders/style-loader)
+                    { loader: 'style-loader' },
+                    // [css-loader](/loaders/css-loader)
+                    {
+                      loader: 'css-loader',
+                    },
+                    // [sass-loader](/loaders/sass-loader)
+                    { loader: 'sass-loader' }
+                  ]
             }
         ]
     },
